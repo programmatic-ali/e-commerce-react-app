be86b706c381b6f41e3879051350098e28960bca
alisena <alisenamudaber@gmail.com>
2020-09-18 12:52:04 +0100

    initial commit: created project, and started homepage

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className="menu-directory">
+            <div className="item-category">
+                <div className="content">
+                    <h1 className="title">WALLETS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="item-category">
+                <div className="content">
+                    <h1 className="title">BAGS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="item-category">
+                <div className="content">
+                    <h1 className="title">WATCHES</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="item-category">
+                <div className="content">
+                    <h1 className="title">GLASSES</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
