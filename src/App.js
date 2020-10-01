ffc18c2294aea0c3ab24f3af7ad81577ad333ad6
alisena <alisenamudaber@gmail.com>
2020-10-02 00:39:23 +0100

    built header and worked on forms

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component';
 
 import './pages/homepage/homepage.styles.scss';
 
@@ -12,9 +14,11 @@ import './pages/homepage/homepage.styles.scss';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/bags' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
